719332a05c99f1d47dccae047292cc6ae6e428aa
matao <muxter.matao@gmail.com>
2017-09-26 10:13:03 +0800

    OPT - format

--- chapter2.scm
+++ chapter2.scm
@@ -1,30 +1,32 @@
 #lang planet neil/sicp
 
 ; Exercise 1
+; Calculate followings using Scheme interpreter.
 
-; (1+39) * (53-45)
+; 1. (1+39) * (53-45)
 (* (+ 1 39) (- 53 45))
 
-; (1020 / 39) + (45 * 2)
+; 2. (1020 / 39) + (45 * 2)
 (+ (/ 1020 39) (* 45 2))
 
-; Sum of 39, 48, 72, 23, and 91
+; 3. Sum of 39, 48, 72, 23, and 91
 (+ 39 48 72 23 91)
 
-; Average of 39, 48, 72, 23, and 91 as a floating point.
+; 4. Average of 39, 48, 72, 23, and 91 as a floating point.
 (exact->inexact (/ (+ 39 48 72 23 91) 5))
 
 
 ; Exercise 2
+; Calculate following values using Scheme interpreter.
 
-; circle ratio, π
+; 1. circle ratio, π
 (* 4 (atan 1.0))
 
-; exp(2/3)
+; 2. exp(2/3)
 (exp 2/3)
 
-; 3 to the power of 4
+; 3. 3 to the power of 4
 (expt 3 4)
 
-; logarithm of 1000
+; 4. logarithm of 1000
 (log 1000)
